6d3f3ccc5a6786fb44bda4c552b46e49b5900868
Prasath Nadarajah <prasath@humanmade.com>
2019-12-25 10:36:47 +0530

    7164: Add option data for netstorage files.

--- inc/class-wp-cli-command.php
+++ inc/class-wp-cli-command.php
@@ -113,6 +113,21 @@ class WP_CLI_Command extends \WP_CLI_Command {
 					$options['context'] = 'page';
 					$options['action']  = 'wp_cli_netstorage_publish';
 				}
+			} else {
+				$parsed_url = wp_parse_url( $url );
+				if ( ! empty( $parsed_url ) ) {
+					$args = array(
+						'meta_key'   => 'netstorage_path',
+						'meta_value' => substr( $parsed_url['path'], 1 ),
+						'post_type'  => 'netstorage-file',
+					);
+
+					$query = new \WP_Query( $args );
+					if ( ! empty( $query->post ) ) { // Get the first post assuming no posts will have the same path.
+						$options['context'] = 'netstorage-file';
+						$options['action']  = 'wp_cli_netstorage_publish';
+					}
+				}
 			}
 
 			do_action( 'ns_wp_cli_export_page', $post_id, $options );
